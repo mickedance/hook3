1fb8ed4fff87b2a242f740c6ab3eaedffa2e6214
mickedance <mickedance@hotmail.com>
2022-09-21 13:55:41 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import  ReactDOM  from "react-dom";
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+ReactDOM.render(<div className="alert alert-info">Hello u</div>, document.getElementById("root"))
